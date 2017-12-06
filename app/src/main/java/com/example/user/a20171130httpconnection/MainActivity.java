ae800a4a421e7293f4ad7f689839780b5756bd4a
Joe <s1041672@gm.pu.edu.tw>
2017-12-06 23:15:59 +0800

    20171130picturedownloadFail

---
+++ app/src/main/java/com/example/user/a20171130httpconnection/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.user.a20171130httpconnection;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+import java.io.*;
+import java.net.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Myclass().execute();
+
+    }
+
+    Bitmap bitmap;
+    private  class Myclass extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            bitmap = DownloadImage("https://www.streetcar.org/wp-content/uploads/IMG_1289-1.jpg");
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            img.setImageBitmap(bitmap);
+
+        }
+
+    }
+
+    private InputStream OpenHttpConnection(String urlString) throws IOException {
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        } catch (Exception ex) {
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
+    private Bitmap DownloadImage(String URL) {
+        Bitmap bitmap = null;
+        InputStream in = null;
+
+        try {
+            in = OpenHttpConnection(URL);
+            bitmap = BitmapFactory.decodeStream(in);
+            in.close();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return bitmap;
+    }
+
+}
+
+
+
